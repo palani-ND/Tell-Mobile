bc3e25599467d3e3b01da15eb07e9a9089212ed8
palani <palani@navadhiti.com>
2023-12-22 08:52:31 +0530

    initial commit , added webview and tell-portal link

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, View,SafeAreaView } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+    
+      <WebView
+        source={{ uri: 'https://dev.tell.navadhiti.com/' }}
+        style={{ flex: 1 }}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
